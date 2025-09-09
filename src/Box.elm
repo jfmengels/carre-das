9b356c1c1252e7685ca3fb49c32d0e5d145b5e40
Jeroen Engels <jfm.engels@gmail.com>
2025-09-10 00:46:48 +0200

    Detect words even next to punctuation

--- src/Box.elm
+++ src/Box.elm
@@ -5,7 +5,8 @@ import Element exposing (Element)
 import Element.Background as Background
 import Element.Font as Font
 import Element.Input
-import Types exposing (Color)
+import Regex exposing (Regex)
+import Types exposing (Color(..))
 
 
 box : Color -> { onPress : Maybe msg, label : String } -> Element msg
@@ -38,22 +39,44 @@ viewBoxContent text =
                 [ Element.text "En réserve" ]
 
              else
-                text
-                    |> String.split " "
-                    |> List.map wordToElement
-                    |> List.intersperse (Element.text " ")
+                wordToElement text
             )
         ]
 
 
-wordToElement : String -> Element msg
-wordToElement word =
-    case Color.fromString word of
-        Just color ->
-            Element.el [ Font.color (Color.backgroundColor color) ] (Element.text word)
+wordToElement : String -> List (Element msg)
+wordToElement inputStr =
+    case Regex.find colorWordsRegex inputStr of
+        [] ->
+            [ Element.text inputStr ]
 
-        Nothing ->
-            Element.text word
+        matches ->
+            List.foldl
+                (\match ( lastIndex, res ) ->
+                    ( match.index + String.length match.match
+                    , highlight match.match :: Element.text (String.slice lastIndex match.index inputStr) :: res
+                    )
+                )
+                ( 0, [] )
+                matches
+                |> (\( lastIndex, res ) -> Element.text (String.slice lastIndex (String.length inputStr) inputStr) :: res)
+                |> List.reverse
+
+
+colorWordsRegex : Regex
+colorWordsRegex =
+    Regex.fromStringWith { caseInsensitive = True, multiline = False } ("\\b(" ++ String.join "|" colors ++ ")\\b")
+        |> Maybe.withDefault Regex.never
+
+
+highlight : String -> Element msg
+highlight color =
+    Element.el [ Font.color (Color.backgroundColor (Color.fromString color |> Maybe.withDefault Blue)) ] (Element.text color)
+
+
+colors : List String
+colors =
+    [ "bleu", "vert", "rouge", "jaune" ]
 
 
 white : Element.Color
